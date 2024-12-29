74428e781b0b6c8a94ed82b8c2406ab162bd4434
LeeYuSung <salamander011017@gmail.com>
2024-12-29 22:25:20 +0900

    12/29

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,73 +2,72 @@ import java.io.*;
 import java.util.*;
 
 public class test {
-    static int n;
-    static int[][] hint;
+    static int N;
+    static int protein, fat, carbo, vitamin;
+    static int[][] ingredients;
     static int answer;
+    static ArrayList<Integer> used, answerUsed;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        hint = new int[n][3];
+        StringTokenizer st;
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            hint[i][0] = Integer.parseInt(st.nextToken()); // Number
-            hint[i][1] = Integer.parseInt(st.nextToken()); // Strike
-            hint[i][2] = Integer.parseInt(st.nextToken()); // Ball
+        // 입력받기
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        protein = Integer.parseInt(st.nextToken());
+        fat = Integer.parseInt(st.nextToken());
+        carbo = Integer.parseInt(st.nextToken());
+        vitamin = Integer.parseInt(st.nextToken());
+
+        ingredients = new int[N][5];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                ingredients[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
 
-        answer = 0;
-        recur(0, 100);
-        System.out.println(answer);
-    }
+        // 초기화
+        answer = Integer.MAX_VALUE;
+        used = new ArrayList<>();
+        answerUsed = new ArrayList<>();
 
-    static boolean checker(int idx, int number) {
-        int _number = hint[idx][0];
-        int _strike = hint[idx][1];
-        int _ball = hint[idx][2];
+        // 재귀 호출 시작
+        recur(0, 0, 0, 0, 0, 0);
 
-        int strike = 0;
-        int ball = 0;
-
-        int _A = _number / 100;
-        int _B = (_number % 100) / 10;
-        int _C = _number % 10;
-
-        int A = number / 100;
-        int B = (number % 100) / 10;
-        int C = number % 10;
-
-        // Validation for digits
-        if (A == 0 || B == 0 || C == 0) return false;
-        if (A == B || A == C || B == C) return false;
-
-        // Check strike
-        if (A == _A) strike++;
-        if (B == _B) strike++;
-        if (C == _C) strike++;
-
-        // Check ball
-        if (A == _B || A == _C) ball++;
-        if (B == _A || B == _C) ball++;
-        if (C == _A || C == _B) ball++;
-
-        return strike == _strike && ball == _ball;
-    }
-
-    static void recur(int idx, int number) {
-        if (idx == n) {
-            answer++;
-            recur(0, number + 1);
-            return;
-        }
-
-        if (number == 1000) return;
-
-        if (checker(idx, number)) {
-            recur(idx + 1, number);
+        // 결과 출력
+        if (!answerUsed.isEmpty()) {
+            System.out.println(answer);
+            for (int num : answerUsed) {
+                System.out.print(num + " ");
+            }
         } else {
-            recur(0, number + 1);
+            System.out.println(-1);
         }
+    }
+
+    static void recur(int idx, int p, int f, int c, int v, int price) {
+        if (p >= protein && f >= fat && c >= carbo && v >= vitamin) {
+            if (price < answer) {
+                answer = price;
+                answerUsed = new ArrayList<>(used); // 현재 조합 저장
+            }
+        }
+
+        if (idx == N) return; // 모든 재료를 다 탐색한 경우 종료
+
+        // 현재 재료를 선택하는 경우
+        used.add(idx + 1); // 재료 번호는 1부터 시작하므로 +1
+        recur(idx + 1,
+                p + ingredients[idx][0],
+                f + ingredients[idx][1],
+                c + ingredients[idx][2],
+                v + ingredients[idx][3],
+                price + ingredients[idx][4]);
+        used.remove(used.size() - 1); // 백트래킹을 위해 제거
+
+        // 현재 재료를 선택하지 않는 경우
+        recur(idx + 1, p, f, c, v, price);
     }
 }
